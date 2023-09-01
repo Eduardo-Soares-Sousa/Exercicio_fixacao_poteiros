177562a08289330f3017c9440b8fcf3f9802df9d
Eduardo-Soares-Sousa <eduardo_soares001@outlook.com>
2023-09-01 15:53:22 -0300

    Commit intermediário

---
+++ Exer_fix2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct aluno{
+    char nome[50];
+    float media;
+    int faltas;
+};
+
+
+int main(int argc, char const *argv[]){
+
+    struct aluno *dados;
+    
+    
+    
+    return 0;
+}
